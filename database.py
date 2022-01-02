053e91faca31ff3c7078097e9e1f7ca5bf0fedbf
Filipe Morais <afilipem@gmail.com>
2022-01-02 17:55:37 -0300

    modify movies_table and add users and watched

--- database.py
+++ database.py
@@ -5,14 +5,20 @@ from sqlite3.dbapi2 import Cursor
 
 
 CREATE_MOVIES_TABLE = """CREATE TABLE IF NOT EXISTS movies(
+    id INTEGER PRIMARY KEY,
     title TEXT,
     release_timestamp REAL,
-    watched INTEGER
     );"""
 
-CREATE_WATCHLIST_TABLE = """CREATE TABLE IF NOT EXISTS watched(
-    watchername TEXT,
-    title TEXT
+CREATE_USERS_TABLE = """CREATE TABLE IF NOT EXISTS users (
+    username TEXT PRIMARY KEY
+);"""
+
+CREATE_WATCHED_TABLE = """CREATE TABLE IF NOT EXISTS watched(
+    user_username TEXT,
+    movie_id INTEGER,
+    FOREIGN KEY(user_username) REFERENCES users(username)
+    FOREIGN KEY(movie_id) REFERENCES
 );"""
 
 INSERT_MOVIES = "INSERT INTO movies (title, release_timestamp, watched) VALUES (?, ?, 0);"
@@ -29,7 +35,8 @@ connection = sqlite3.connect("data.db")
 def create_tables():
     with connection:
         connection.execute(CREATE_MOVIES_TABLE)
-        connection.execute(CREATE_WATCHLIST_TABLE)
+        connection.execute(CREATE_WATCHED_TABLE)
+        connection.execute(CREATE_USERS_TABLE)
     pass
 
 def add_movie(title, release_timestamp):
